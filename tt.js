d4e1813f7939a9c37a12ba142280e8f4fac1ab5a
kaos-it <aqasif5566@gmail.com>
2024-04-22 22:59:28 +0400

    new

---
+++ tt.js
@@ -0,0 +1,7 @@
+let string = "her hansi bir string"; // her hansi bir string ola biler
+
+if (string.length >= 10 && string.length <= 20) {
+    console.log("TRUE");
+} else {
+    console.log("FALSE");
+}
